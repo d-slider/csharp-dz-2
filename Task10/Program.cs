724f98498ce33e9eebe4f2b130c6dba2f0b0e4c9
d-slider <sliderdslider@gmail.com>
2022-09-04 12:40:25 +0300

    Homework is done!

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Clear();
+Console.Write("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int digit;
+
+if (number > 99 && number < 1000)
+{
+    digit = number % 100;  //  456 -> 56
+    digit = digit / 10;    //  56  -> 5
+
+    Console.Write($"Вторая цифра числа: {number} = {digit}");
+}
+else
+    Console.Write("Вы ввели не трёхзначное число!");
--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Clear();
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int num = number;
+int digit;
+
+if (num < 0) num = num * (-1); // Если число отрицательное, преобразуем его в такое же положительное
+
+if (num < 100)
+{
+    Console.Write($"Третьей цифры нет числа {number} нет");
+}
+else
+{
+    while (num > 999)
+    {
+        num = num / 10;
+    }
+    digit = num % 10;  //  326 -> 6
+    Console.Write($"Третья цифра числа: {number} = {digit}");
+}
