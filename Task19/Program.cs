de5c23e9fbedd0f6ab6600f6a50432e3b2e0e9f5
d-slider <sliderdslider@gmail.com>
2022-09-10 18:43:33 +0300

    Homework is done

---
+++ Task19/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Clear();
+Console.Write("Введите пятизначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int Revers(int number) // Описываем метод реверса числа
+{
+    int result = 0;
+
+    while (number > 0)
+    {
+        result *= 10;
+        result += number % 10;
+        number /= 10;
+    }
+    return result;
+}
+
+if (num >= 10000 && num <= 99999)
+{
+    int reversNum = Revers(num); // Метод возвращает реверс введённого числа
+
+    if (num == reversNum) Console.WriteLine("да");
+    else Console.WriteLine("нет");
+}
+else Console.WriteLine("Введено некоректное число");
---
+++ Task23/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+Console.Write("Введите число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void CubeTable(int digit)
+{
+    int count = 1;
+
+    while (count <= digit)
+    {
+        Console.WriteLine($"|{count} - {count * count * count, 3}|");
+        count++;
+    }
+}
+CubeTable(number);
